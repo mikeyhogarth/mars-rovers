1398f70d32a82dbdbd17a927db4c4fcd377da422
Mike Hogarth <mikehogarth20@hotmail.com>
2019-07-20 10:55:10 +0100

    rename query

--- src/graphql/schema/query.js
+++ src/graphql/schema/query.js
@@ -2,13 +2,13 @@ import { gql } from "apollo-server-lambda";
 
 export const typeDefs = gql`
   type Query {
-    getManifestForRover(rover: Rover): Manifest
+    manifests(rover: Rover!): Manifest!
   }
 `;
 
 export const resolvers = {
   Query: {
-    getManifestForRover: async (_, { rover }, { dataSources }) => {
+    manifests: async (_, { rover }, { dataSources }) => {
       return dataSources.nasaMarsRoversAPI.getManifestForRover(rover);
     }
   }
